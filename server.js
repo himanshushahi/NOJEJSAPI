7218c136bb052ffd5f49091a6661afaf1e7966f2
Himanshu <himanshushahi0478@gmail.com>
2023-05-02 14:24:32 +0530

    change the cors

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ const app = express();
 // for connecting the server to the database
 connectDB();
 // to get the json data from frontend
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URI],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use(cookieParser());
