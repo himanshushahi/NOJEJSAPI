5810205073ca2133f142102d95be90bba9b848d4
Himanshu <himanshushahi0478@gmail.com>
2023-05-02 22:02:07 +0530

    change the cors

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const app = express();
 // for connecting the server to the database
 connectDB();
 // to get the json data from frontend
-app.use(cors({
-  origin: process.env.FRONTEND_URI,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
-
 app.use(express.json());
-
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URI],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // this is for the routes
 app.use("/user", loginRouter);
