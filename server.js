1d07fcf80ec1fbed8812e92153588598cceb94b6
Himanshu <himanshushahi0478@gmail.com>
2023-05-03 08:05:11 +0530

    change the cors setting in login router

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use(
   })
 );
 
+loginRouter.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URI);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 // this is for the routes
 app.use("/user", loginRouter);
 app.use("/task", taskRouter);
